799cf14c865db50e451c51944aa839dda21e7a3a
Bijaya Gurung <bijayagurung@192-168-1-9.tpgi.com.au>
2024-11-05 08:50:16 +1100

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Portfolio from './Portfolio';
 import ProjectDetail from './ProjectDetail';
-import {Route,Routes} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
